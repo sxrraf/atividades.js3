a4c7476ced7fe1013911dd0cc9c9801c1a68604b
yara <97001981+sxrraf@users.noreply.github.com>
2024-03-04 13:52:46 -0300

    Add files via upload

---
+++ teste1.js
@@ -0,0 +1,4 @@
+function tabuada(){
+    for(var count=0; count<=10 ; count++)
+    document.write("1 x "+count+" = " + (10*count) + "<br />")
+} 
---
+++ teste2.js
@@ -0,0 +1,7 @@
+function numeros_pares(){
+    for (var num=1; num <=20; num++){
+if(num % 2 === 0) {
+     document.write(num + "<br>")
+}
+    }
+}
